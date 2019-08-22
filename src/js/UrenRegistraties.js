ecd0f8f97aadca554be65688b2611a30aa162527
RGCoes <sigma_fi19@yahoo.com>
2019-08-22 22:59:14 +0200

    tweede commit

--- src/js/UrenRegistraties.js
+++ src/js/UrenRegistraties.js
@@ -1,7 +1,22 @@
 
 
 function registreer(){
-    var a =document.getElementById("hoi").value;
-    console.log("value: " + a);
-    alert("waarde"+a);
+    var totaal = 0;
+    var i;
+    alert("waarde" + a);
+
+    var a = document.getElementsByClassName("MAAND" );
+    console.log(a.length);
+
+    for( i=0;   i<=30;    i++ ) {
+        var b = a[i].value;
+        var c = Number(b);
+        totaal += c;
+        console.log("value: " + totaal);
+
+    }
+
+    document.getElementById("totaal").value = totaal;
+    totaal = 0;
+
 }
